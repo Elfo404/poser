57b1c62a0a438fafc563075b8990d7363a0cb555
Matteo Manchi <matteo.manchi@gmail.com>
2018-02-27 21:34:41 +0100

    Copy to clipboard with tooltip

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
-console.log('Be a poser!');
+import Clipboard from 'clipboard';
+import Tooltip from 'tooltip.js';
+
+const clipboard = new Clipboard('button');
+
+clipboard.on('success', e => {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    const instance = new Tooltip(e.trigger, {
+        title: 'Copied!',
+        placement: 'top',
+        trigger: 'manual',
+    });
+    instance.show();
+
+    setTimeout(() => instance.dispose(), 1000);
+});
